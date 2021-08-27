e5bcdf3e9ad4f873cecac98ef0ba79dbf7d51fdc
Omar Ouhra <omarmaroc26@gmail.com>
2021-08-27 09:16:09 +0100

    init

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>A Digital Agency in Stockholm</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+    </div>
+  );
+}
