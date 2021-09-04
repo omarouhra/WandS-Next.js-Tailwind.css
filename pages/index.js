c3b2f0a7ffcd854d34dd8d990e1a775684c7303b
Omar Ouhra <omarmaroc26@gmail.com>
2021-09-04 17:16:36 +0100

    Layout fix

--- pages/index.js
+++ pages/index.js
@@ -137,185 +137,186 @@ export default function Home() {
 
           <Companies />
         </section>
-
-        {/* Paragraph */}
-        <section className='padding'>
-          <Paragraph
-            firstPart='We guide and assist our clients in designing and developing '
-            span='seamless'
-            secondPart='digital experiences'
-            center={true}
-            blueSpane={true}
-          />
-        </section>
-
-        {/* Pics section */}
-        <Picture
-          image={
-            <div className='relative w-[90vw] h-[20vh] md:h-[30vh] lg:h-[40vh] lg:max-w-4xl'>
-              <Image
-                data-aos='fade-in'
-                data-aos-duration='1000'
-                src={image1}
-                layout='fill'
-                objectFit='cover'
-              />{" "}
-            </div>
-          }
-        />
-
-        {/* About us */}
-        <section
-          id='aboutUs'
-          className='padding md:max-w-6xl mx-auto space-y-16 px-6'>
-          <article data-aos='fade-in' data-aos-duration='500'>
-            <p className='text-lg max-w-xl'>
-              We help you succeed with your digital efforts by developing
-              <span className='span'> solutions</span> tailored for your
-              business
-            </p>
-            <button className='border px-3 py-1 rounded-sm border-black  mt-6 '>
-              Contact us
-            </button>
-          </article>
-          <article
-            data-aos='fade-in'
-            data-aos-duration='500'
-            className=' flex flex-col justify-center items-end'>
-            <Title title={"About us"} alignementRight={true} />
-            <p className=' text-gray text-sm lg:text-md text-right  my-6 max-w-xl leading-loose md:leading-loose lg:leading-loose'>
-              Our teams thrive on challenges and we enjoy crafting world class
-              solutions that enable start-ups and established companies to
-              streamline internal processes and build great products.
-            </p>
-          </article>
-        </section>
-
-        {/* Services */}
-        <section id='services' className='padding md:max-w-6xl mx-auto px-6'>
-          <small className='text-xs md:text-sm  text-gray-300'>
-            Take a look on{" "}
-          </small>
-          <Title
-            title='What do we bring to the table ?'
-            alignementRight={false}
-          />
-          <section className=' grid gap-7 mt-12 md:grid-cols-2 md:gap-16 '>
-            {ServiceData?.map(({ id, icon, title, paragraph }) => (
-              <Service
-                key={id}
-                icon={icon}
-                title={title}
-                paragraph={paragraph}
-              />
-            ))}
+        <main>
+          {/* Paragraph */}
+          <section className='padding'>
+            <Paragraph
+              firstPart='We guide and assist our clients in designing and developing '
+              span='seamless'
+              secondPart='digital experiences'
+              center={true}
+              blueSpane={true}
+            />
           </section>
-        </section>
 
-        {/* Pics section */}
-        <Picture
-          alignementRight={true}
-          image={
-            <div className='relative w-[80vw] h-[25vh] md:h-[40vh]  lg:h-[50vh] lg:max-w-4xl'>
-              <Image
-                data-aos='fade-in'
-                data-aos-duration='1000'
-                src='https://images.unsplash.com/photo-1560264418-c4445382edbc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80'
-                layout='fill'
-                objectFit='cover'
-              />
-            </div>
-          }
-        />
-
-        {/* Paragraph  */}
-        <section className='padding'>
-          <Paragraph
-            firstPart='We are a team of naturally curious '
-            span='designers & developers'
-            secondPart='with different skills and experiences'
-            blueSpane={true}
+          {/* Pics section */}
+          <Picture
+            image={
+              <div className='relative w-[90vw] h-[20vh] md:h-[30vh] lg:h-[40vh] lg:max-w-4xl'>
+                <Image
+                  data-aos='fade-in'
+                  data-aos-duration='1000'
+                  src={image1}
+                  layout='fill'
+                  objectFit='cover'
+                />{" "}
+              </div>
+            }
           />
-        </section>
 
-        {/* Slider */}
-        <Slider />
-
-        {/* Projects */}
-        <section id='projects' className='padding md:max-w-6xl mx-auto px-6'>
-          <Project
-            projectImage={projectData[projectId].image}
-            smallTitle={projectData[projectId].description}
-            title={projectData[projectId].title}
-            paragraph={projectData[projectId].paragraph}
-            link={projectData[projectId].link}
-            setProjectId={setProjectId}
-            id={projectId}
-          />
-        </section>
-
-        {/* Blog section */}
-        <section className='padding md:max-w-6xl mx-auto px-6 '>
-          <section className='py-12 px-6 bg-gray-light flex flex-col space-y-16 lg:flex-row lg:space-y-0 lg:space-x-12'>
-            <section>
-              <Title title='Our latest news and articles' />
-              <p className='text-sm text-gray-400 leading-relaxed my-5'>
-                For more information regarding our technologies and our work,
-                please visit our tech blog.
+          {/* About us */}
+          <section
+            id='aboutUs'
+            className='padding md:max-w-6xl mx-auto space-y-16 px-6'>
+            <article data-aos='fade-in' data-aos-duration='500'>
+              <p className='text-lg max-w-xl'>
+                We help you succeed with your digital efforts by developing
+                <span className='span'> solutions</span> tailored for your
+                business
               </p>
-              <Button link='All Articles' />
-            </section>
+              <button className='border px-3 py-1 rounded-sm border-black  mt-6 '>
+                Contact us
+              </button>
+            </article>
+            <article
+              data-aos='fade-in'
+              data-aos-duration='500'
+              className=' flex flex-col justify-center items-end'>
+              <Title title={"About us"} alignementRight={true} />
+              <p className=' text-gray text-sm lg:text-md text-right  my-6 max-w-xl leading-loose md:leading-loose lg:leading-loose'>
+                Our teams thrive on challenges and we enjoy crafting world class
+                solutions that enable start-ups and established companies to
+                streamline internal processes and build great products.
+              </p>
+            </article>
+          </section>
 
-            <section className='flex flex-col space-y-16 lg:flex-row lg:space-y-0 lg:space-x-12'>
-              {blogData.map(({ id, image, date, title }) => (
-                <Blog key={id} src={image} date={date} title={title} />
+          {/* Services */}
+          <section id='services' className='padding md:max-w-6xl mx-auto px-6'>
+            <small className='text-xs md:text-sm  text-gray-300'>
+              Take a look on{" "}
+            </small>
+            <Title
+              title='What do we bring to the table ?'
+              alignementRight={false}
+            />
+            <section className=' grid gap-7 mt-12 md:grid-cols-2 md:gap-16 '>
+              {ServiceData?.map(({ id, icon, title, paragraph }) => (
+                <Service
+                  key={id}
+                  icon={icon}
+                  title={title}
+                  paragraph={paragraph}
+                />
               ))}
             </section>
           </section>
-        </section>
 
-        {/* Contact us */}
-        <section
-          data-aos='fade-in'
-          data-aos-duration='500'
-          id='contacts'
-          className='padding md:max-w-6xl mx-auto px-6 '>
-          <section className='py-12 px-6 md:px-12 bg-blue-dark text-white shadow-2xl md:flex md:space-x-12'>
-            <p className=' text-3xl leading-loose mb-10 md:w-2/3'>
-              NOW ABOUT YOUR <br />
-              <span className='span'>PROJECT...</span>
-            </p>
-            <section>
-              <p className='text-sm leading-loose mb-12 md:mb-10'>
-                Do you want to discuss an idea, develop your technical platform
-                or have thoughts on how to begin your digital journey? Do not
-                hesitate to get in touch.
-              </p>
-              <a href='mailto=contact@will&skill.se' className='underline'>
-                contact@will&skill.se <span className='animate-pulse'> > </span>{" "}
-              </a>
+          {/* Pics section */}
+          <Picture
+            alignementRight={true}
+            image={
+              <div className='relative w-[80vw] h-[25vh] md:h-[40vh]  lg:h-[50vh] lg:max-w-4xl'>
+                <Image
+                  data-aos='fade-in'
+                  data-aos-duration='1000'
+                  src='https://images.unsplash.com/photo-1560264418-c4445382edbc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80'
+                  layout='fill'
+                  objectFit='cover'
+                />
+              </div>
+            }
+          />
+
+          {/* Paragraph  */}
+          <section className='padding'>
+            <Paragraph
+              firstPart='We are a team of naturally curious '
+              span='designers & developers'
+              secondPart='with different skills and experiences'
+              blueSpane={true}
+            />
+          </section>
+
+          {/* Slider */}
+          <Slider />
+
+          {/* Projects */}
+          <section id='projects' className='padding md:max-w-6xl mx-auto px-6'>
+            <Project
+              projectImage={projectData[projectId].image}
+              smallTitle={projectData[projectId].description}
+              title={projectData[projectId].title}
+              paragraph={projectData[projectId].paragraph}
+              link={projectData[projectId].link}
+              setProjectId={setProjectId}
+              id={projectId}
+            />
+          </section>
+
+          {/* Blog section */}
+          <section className='padding md:max-w-6xl mx-auto px-6 '>
+            <section className='py-12 px-6 bg-gray-light flex flex-col space-y-16 lg:flex-row lg:space-y-0 lg:space-x-12'>
+              <section>
+                <Title title='Our latest news and articles' />
+                <p className='text-sm text-gray-400 leading-relaxed my-5'>
+                  For more information regarding our technologies and our work,
+                  please visit our tech blog.
+                </p>
+                <Button link='All Articles' />
+              </section>
+
+              <section className='flex flex-col space-y-16 lg:flex-row lg:space-y-0 lg:space-x-12'>
+                {blogData.map(({ id, image, date, title }) => (
+                  <Blog key={id} src={image} date={date} title={title} />
+                ))}
+              </section>
             </section>
           </section>
-        </section>
 
-        {/* Carrer section */}
-        <section className='pb-12 md:max-w-6xl mx-auto px-6  flex flex-col space-y-12 md:flex-row md:space-y-0 md:justify-between'>
-          <p data-aos='fade-in' data-aos-duration='500'>
-            Looking for a <span className='span'>job?</span>
-          </p>
-
+          {/* Contact us */}
           <section
             data-aos='fade-in'
             data-aos-duration='500'
-            className='md:w-1/2'>
-            <p className='text-gray-dark text-xs md:text-md leading-loose mb-8 '>
-              We are constantly looking for more colleagues who want to help us
-              help our customers build the IT systems of the future.
-            </p>
-            <Button link='Carrer Page' />
+            id='contacts'
+            className='padding md:max-w-6xl mx-auto px-6 '>
+            <section className='py-12 px-6 md:px-12 bg-blue-dark text-white shadow-2xl md:flex md:space-x-12'>
+              <p className=' text-3xl leading-loose mb-10 md:w-2/3'>
+                NOW ABOUT YOUR <br />
+                <span className='span'>PROJECT...</span>
+              </p>
+              <section>
+                <p className='text-sm leading-loose mb-12 md:mb-10'>
+                  Do you want to discuss an idea, develop your technical
+                  platform or have thoughts on how to begin your digital
+                  journey? Do not hesitate to get in touch.
+                </p>
+                <a href='mailto=contact@will&skill.se' className='underline'>
+                  contact@will&skill.se{" "}
+                  <span className='animate-pulse'> > </span>{" "}
+                </a>
+              </section>
+            </section>
           </section>
-        </section>
 
+          {/* Carrer section */}
+          <section className='pb-12 md:max-w-6xl mx-auto px-6  flex flex-col space-y-12 md:flex-row md:space-y-0 md:justify-between'>
+            <p data-aos='fade-in' data-aos-duration='500'>
+              Looking for a <span className='span'>job?</span>
+            </p>
+
+            <section
+              data-aos='fade-in'
+              data-aos-duration='500'
+              className='md:w-1/2'>
+              <p className='text-gray-dark text-xs md:text-md leading-loose mb-8 '>
+                We are constantly looking for more colleagues who want to help
+                us help our customers build the IT systems of the future.
+              </p>
+              <Button link='Carrer Page' />
+            </section>
+          </section>
+        </main>
         {/* footer section */}
         <footer className='pt-12 pb-4 md:max-w-6xl mx-auto px-6 '>
           <section className='py-12 px-6 lg:px-12 bg-gray-light '>
